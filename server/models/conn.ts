08eb46092de6984e5a88851e922c6acd812182bf
Lucas Calvetti <lic.calvo099@gmail.com>
2023-10-31 22:09:17 -0300

    changed postgres connection

--- server/models/conn.ts
+++ server/models/conn.ts
@@ -1,19 +1,21 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-export const sequelize = new Sequelize({
-    dialect: "postgres",
-    username: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
-    port: 5432,
-    host: process.env.POSTGRES_HOST,
-    ssl: true, //para que se conecte de forma segura
-    // esto es necesario para que corra correctamente
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false,
-        },
-    },
-});
+// export const sequelize = new Sequelize({
+//     dialect: "postgres",
+//     username: process.env.POSTGRES_USERNAME,
+//     password: process.env.POSTGRES_PASSWORD,
+//     database: process.env.POSTGRES_DATABASE,
+//     port: 5432,
+//     host: process.env.POSTGRES_HOST,
+//     ssl: true, //para que se conecte de forma segura
+//     // esto es necesario para que corra correctamente
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false,
+//         },
+//     },
+// });
+
+export const sequelize = new Sequelize(`postgres://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}.com/${process.env.POSTGRES_DATABASE}`);
